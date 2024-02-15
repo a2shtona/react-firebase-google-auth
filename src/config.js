e20440ce2d2ce0f753aea3284de1239f8c6b6985
solbest <solbest205@gmail.com>
2024-02-15 10:33:18 -0500

    react-firebase auth example

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export const firebase_api_key = "AIzaSyA9cNRooW5mBn2znMT5MdXad68ozIwqvLg"
+export const authDomain = "reactauth-d78fd.firebaseapp.com"
+export const projectId = "reactauth-d78fd"
+export const storageBucket = "reactauth-d78fd.appspot.com"
+export const messagingSenderId = "837802886330"
+export const appId = "1:837802886330:web:6b98387877312c3a09ae83"
+export const measurementId = "G-J55D0DYPJT"
